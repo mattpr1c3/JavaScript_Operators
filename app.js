37f8bfb8a4f10c82d939bc9a5a7859159ecb7eeb
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-10 10:07:16 -0700

    added line breaks for exercises

--- app.js
+++ app.js
@@ -4,17 +4,25 @@ console.log(
 );
 
 // Exercise 1
+console.log("Exercise 1:\n==========\n");
+// YOUR CODE HERE
 let a = 20;
 let b = 4;
 let add = a + b;
 let minus = a - b;
 let multiply = a * b;
 let dividing = a / b;
-console.log(add);
 
-// YOUR CODE HERE
+
+
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
 
 // Exercise 2
+console.log("Exercise 2:\n==========\n");
+// YOUR CODE HERE
 let num = 11;
 let str = "11";
 let str2 = "eleven";
@@ -23,20 +31,22 @@ let firstName = "Frodo";
 let lastName = "Baggins";
 
 
+
 console.log(num + str);
 console.log(num + str2);
 console.log(num + isPresent);
 console.log(firstName + num);
 console.log(isPresent + str);
 console.log(firstName + lastName);
-
-// YOUR CODE HERE
-
 // Exercise 3
+console.log("Exercise 3:\n==========\n");
+// YOUR CODE HERE
 let val = 5;
 let str3 = "5";
 let str4 = "five";
 let isAwake = false;
+
+
 
 console.log(val == str3);
 console.log(val === str3);
@@ -47,5 +57,3 @@ console.log(0 == false);
 console.log(0 === false);
 console.log(0 != false);
 console.log(0 !== false);
-
-// YOUR CODE HERE
